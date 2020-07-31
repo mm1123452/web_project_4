460e1a5140c8fa4ae724856d9802bb12f101239b
Mabel <macmota@hotmail.com>
2020-07-30 21:21:13 -0400

    update class instance

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 
 //Form data
  const editPopup = document.querySelector(".popup_type_edit");
-export let nameInput = editPopup.querySelector(".popup__name");
-export let aboutInput = editPopup.querySelector(".popup__about");
+export const  nameInput = editPopup.querySelector(".popup__name");
+export const aboutInput = editPopup.querySelector(".popup__about");
 
 //DOM Elements
 export const profile = document.querySelector(".profile");
